85a9dafcccd724ac01f689201e23264f9a723db8
dudushy <eduardotalarico1337@gmail.com>
2024-01-19 22:00:35 -0300

    Update build_n_deploy.sh

--- build_n_deploy.sh
+++ build_n_deploy.sh
@@ -23,6 +23,6 @@ echo $TITLE "setup config done!"
 #* main
 echo $TITLE "run"
 ng build --base-href $PAGE_URL
-npx angular-cli-ghpages --dir=dist/$PROJECT_NAME --message=$COMMIT_MESSAGE
+npx angular-cli-ghpages --dir=dist/$PROJECT_NAME/browser --message=$COMMIT_MESSAGE
 
 echo $TITLE "finished."
